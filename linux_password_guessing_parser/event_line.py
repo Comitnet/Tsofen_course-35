f833b9b46eec939b37eebc653d1c29c503f48aba
anisbi <anis.bishara1@gmail.com>
2018-01-17 12:13:53 +0200

    Added 3 types of events (valid/invalid/successful login)

--- linux_password_guessing_parser/event_line.py
+++ linux_password_guessing_parser/event_line.py
@@ -1,16 +1,33 @@
 from enum import Enum
 
 class Attempt_Type(Enum):
-    FAILED_PASSWORD = 1
+    FAILED_LOGIN      = 1
     ACCEPTED_PASSWORD = 2
 
-class event_line:
+class Event_Type(Enum):
+    FAIL_WRONG_USER   = 1
+    FAIL_RIGHT_USER   = 2
+    SUCCESSFUL_ACCESS = 3
 
-  def __init__(self, event_time, machine_ip_address, attempt_type):
-    self.event_time = event_time
+class Event_Line:
+
+  def __init__(self, event_time, machine_ip_address, attempt_type, valid_username):
+    self.event_time         = event_time
     self.machine_ip_address = machine_ip_address
+    self.valid_username     = valid_username
+
     if attempt_type == "Failed password":
-      self.attempt_type = Attempt_Type.FAILED_PASSWORD
+      self.attempt_type = Attempt_Type.FAILED_LOGIN
     elif attempt_type == "Accepted password":
       self.attempt_type = Attempt_Type.ACCEPTED_PASSWORD
-    
+
+
+
+class Event_Report:
+  def __init__(self, start_line, end_line, event_type):
+    self.start_line = start_line
+    self.end_line   = end_line
+    self.event_type = event_type
+
+  def setEventType(self, event_type):
+    self.event_type = event_type
--- linux_password_guessing_parser/parser.py
+++ linux_password_guessing_parser/parser.py
@@ -1,4 +1,4 @@
-from event_line import event_line
+from event_line import Event_Line, Event_Report, Event_Type, Attempt_Type
 import re
 from datetime import datetime
 
@@ -21,26 +21,27 @@ timestamp_re = re.compile(timestamp_regex)
 ipaddress_re = re.compile(ip_addr_regex)
 
 #trigger if 10 events in 30 seconds
-event_window_secs   = 5
-event_trigger_count = 5
+event_window_secs   = 10
+event_trigger_count = 3
+event_lines = []
 
-def lineToEventLine(line, attempt_type):
+def lineToEventLine(line, attempt_type, valid_username):
   m  = timestamp_re.search(line)
   dt =  datetime.strptime(m.group(0),'%b  %d %H:%M:%S')\
         .replace(year=datetime.now().year)
   m  = ipaddress_re.search(line)
-  el = event_line(dt, m.group(0), attempt_type)
+  el = Event_Line(dt, m.group(0), attempt_type, valid_username)
   event_lines.append(el)
 
 count = 0
-event_lines = []
 with open(log_file_name) as infile:
     for line in infile:
       for f_string in filter_strings:
         if f_string in line:
           print 'found '+f_string+' in line.'
           count += 1
-          lineToEventLine(line, f_string)
+          valid_username = line.find("invalid") == -1
+          lineToEventLine(line, f_string, valid_username)
       '''
         if filter_string in line:
           count += 1
@@ -62,26 +63,36 @@ lower_pointer = 0
 event_report  = None
 events_array  = []
 deltas_array  = []
+event_reports = []
+current_event_type = Event_Type.FAIL_WRONG_USER
 while lower_pointer < len(event_lines) -1:
   lower_pointer += 1
   delta = (event_lines[lower_pointer].event_time - event_lines[upper_pointer].event_time).total_seconds()
-  print "("+str(upper_pointer)+","+str(lower_pointer)+") TYPE: "+str(event_lines[lower_pointer].attempt_type)+" In event: "+str(event_report is not None)
+  print "("+str(upper_pointer)+","+str(lower_pointer)+") TYPE: "+str(event_lines[lower_pointer].attempt_type)+". Valid pass: "+str(event_lines[lower_pointer].valid_username)+" In event: "+str(event_report is not None)
   #print "delta: "+str(delta)+"<="+str(event_window_secs)+" counter: "+str(lower_pointer - upper_pointer)+">="+str(event_trigger_count)
-  
 
 
 
+  #Case no event active and event conditions are triggered => Add new event
   if (delta <= event_window_secs) and (lower_pointer - upper_pointer + 1 >= event_trigger_count) and event_report is None:
     event_report = event_lines[upper_pointer]
     events_array.append(event_report)
     print "("+str(upper_pointer)+","+str(lower_pointer)+")"
-    #print "EEEEEEEE!!!!!!!!!!!!!!EEEEEEEEEEEE"
+
+
+
+  #Case no event active and window got larger than defined window
   elif delta > event_window_secs and event_report is None:
     upper_pointer += 1
 
-  elif (event_lines[lower_pointer].event_time - event_lines[lower_pointer-1].event_time).total_seconds() >= event_window_secs and event_report is not None:
-    print "Event RESET"
+  #Event over/reset
+  elif ((event_lines[lower_pointer].event_time - event_lines[lower_pointer-1].event_time).total_seconds() >= event_window_secs \
+        and event_report is not None) \
+        or (lower_pointer == len(event_lines)-1 and event_report is not None):
     deltas_array.append((event_lines[lower_pointer-1].event_time - event_lines[upper_pointer].event_time).total_seconds())
+    ev = Event_Report(event_lines[upper_pointer], event_lines[lower_pointer-1], current_event_type)
+    event_reports.append(ev)
+    print 'APPENDINGGGGGGGGGGG'
     event_report = None
     upper_pointer = lower_pointer
 
@@ -93,9 +104,9 @@ while lower_pointer < len(event_lines) -1:
   #print str(delta)+" seconds - ipaddr: "+el.machine_ip_address
   #lower_pointer += 1
 
-for event in events_array:
-  print "###start: "+str(event.event_time)
-"""   
+for event_report in event_reports:
+  print "Event ("+str(event_report.event_type)+"): "+str(event_report.start_line.event_time)+" to "+str(event_report.end_line.event_time)
+"""
 if event_report is not None:
   print "Event triggered! "+\
         str(lower_pointer - upper_pointer)+\
